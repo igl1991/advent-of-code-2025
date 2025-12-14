e3c7cadd99e921eae25502538192552d89acea71
Isabelle Glasmacher <isabelle_glasmacher@hotmail.de>
2025-12-14 20:31:09 +0100

    implement trying to find the solution

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -1,3 +1,14 @@
+import { readInput } from "./readInput.ts";
+import { turnLightsOn } from "./turnLightsOn.ts";
+
 export async function day10() {
-    
+    const machines = await readInput();
+    let totalCount = 0;
+    for (const [index, machine] of machines.entries()) {
+        const buttonClicks = turnLightsOn(machine);
+        console.log(index, buttonClicks);
+        totalCount += buttonClicks;
+    }
+
+    console.log('Step 1 - total button clicks', totalCount);
 }
